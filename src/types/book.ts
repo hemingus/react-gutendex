7abcebff1e60c245f6fd3664192b2962a6c49ada
Heming Hanevik <heming.hanevik@gmail.com>
2025-11-29 23:30:27 +0100

    add types to match gutendex api data

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,8 +1,32 @@
-export type Book = {
-    id: number,
-    title: string,
-    authors: string[],
-    imageURL: string,
-    subjects: string[],
-    downloadCount: number
+export interface GutendexResponse {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: Book[];
+}
+
+export interface Book {
+    id: number;
+    title: string;
+    authors: Person[];
+    translators: Person[];
+    editors: Person[];
+    summaries: string[];
+    subjects: string[];
+    bookshelves: string[];
+    languages: string[];
+    copyright: boolean | null;
+    media_type: string;
+    formats: Formats;
+    download_count: number;
+}
+
+export interface Person {
+    name: string;
+    birth_year: number | null;
+    death_year: number | null;
+}
+
+export interface Formats {
+    [mimeType: string]: string;
 }
