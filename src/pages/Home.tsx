2c2838404469f4391f65a03594796f2ad15000e8
Heming Hanevik <heming.hanevik@gmail.com>
2025-11-29 23:31:52 +0100

    change page Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,11 @@
 import BookList from "../components/BookList";
 import { useBooks } from "../hooks/useBookQuery";
-import type { Book } from "../types/book";
 
 export default function Home() {
     const { data, isLoading, isError, error } = useBooks();
     if (isLoading) return <p>Loading...</p>
     if (isError) return <p>{error.toString()}</p>
     
-    const creatBookList = () => {
-        const booklist :Book[] = [];
-        data.forEach((book) => {
-            booklist.push({
-                id: book.id,
-                title: book.title,
-                authors: book.authors,
-                imageURL: book.formats["image/jpeg"],
-                subjects: book.subjects,
-                downloadCount: book.count
-            })
-        })
-    }
-
     return (
         <>
             <h1>Home</h1>
