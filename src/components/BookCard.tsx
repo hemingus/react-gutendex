26a6f946b871083dfdb994646332d7460ef70578
Heming Hanevik <heming.hanevik@gmail.com>
2025-11-30 10:45:41 +0100

    add content to BookCard

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -6,9 +6,11 @@ interface BookCardProps {
 
 export default function BookCard({ book }:BookCardProps) {
     return (
-        <div>
+        <div className="bookCard">
             <h2>{book.title}</h2>
             <img src={book.formats["image/jpeg"]} alt={`${book.title} cover`} />
+            <p>{`Topic: ${book.subjects[0]}`}</p>
+            <p>{`Downloads: ${book.download_count}`}</p>
         </div>
     )
 }
