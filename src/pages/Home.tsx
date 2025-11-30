7dc3f78bc086df1a0b8f82e1f4bbdef33cae0b10
Heming Hanevik <heming.hanevik@gmail.com>
2025-11-30 10:47:10 +0100

    swap useBooks with useBooksByTopic (test)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import BookList from "../components/BookList";
-import { useBooks } from "../hooks/useBookQuery";
+import { useBooks, useBooksByTopic } from "../hooks/useBookQuery";
 
 export default function Home() {
-    const { data, isLoading, isError, error } = useBooks();
+    const { data, isLoading, isError, error } = useBooksByTopic("Fantasy");
     if (isLoading) return <p>Loading...</p>
     if (isError) return <p>{error.toString()}</p>
     
