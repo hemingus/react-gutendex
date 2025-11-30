74dadb1a52508bd28a1863ebcbfd6e61adc2ac9b
Heming Hanevik <heming.hanevik@gmail.com>
2025-11-30 19:56:39 +0100

    add route for book details on id

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import BookDetails from "../components/BookDetails";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <h1>an error occurred</h1>,
         children: [
-            {index: true, element: <Home />}
+            {index: true, element: <Home />},
+            {path: "book/:id", element: <BookDetails />},
+            {path: "*", element: <h1>404 not found</h1>}
         ]
     }
 ])
