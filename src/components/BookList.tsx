8a03755ea40a546d6cc14cc6b3db00891b5194ec
hemingus <heming.hanevik@gmail.com>
2025-12-01 16:17:57 +0100

    refactor BookList to send fav props to BookCard

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,3 +1,4 @@
+import { useFavorites } from "../context/FavoritesContext";
 import type { Book } from "../types/book"
 import BookCard from "./BookCard"
 
@@ -6,10 +7,17 @@ interface BookListProps {
 }
 
 export default function BookList( { booklist }:BookListProps) {
+    const { isFavorite, addFavorite, removeFavorite } = useFavorites();
+
     return (
         <div className="bookList">
             {booklist.map((book) => {
-                return <BookCard key={book.id} book={book} />
+                return <BookCard 
+                    key={book.id} book={book} 
+                    isFavorite={isFavorite} 
+                    addFavorite={addFavorite}
+                    removeFavorite={removeFavorite}
+                    />
             })}
         </div>
     )
