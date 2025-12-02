750171ef6d3202584f13de957b93969f7d04a689
hemingus <heming.hanevik@gmail.com>
2025-12-02 16:43:00 +0100

    add styles to BookList

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -10,7 +10,7 @@ export default function BookList( { booklist }:BookListProps) {
     const { isFavorite, addFavorite, removeFavorite } = useFavorites();
 
     return (
-        <div className="bookList">
+        <div className="w-[90%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-stretch">
             {booklist.map((book) => {
                 return <BookCard 
                     key={book.id} book={book} 
