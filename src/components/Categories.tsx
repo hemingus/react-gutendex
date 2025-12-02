7c028785098c07a138cf8da63333c3c35b8ff226
hemingus <heming.hanevik@gmail.com>
2025-12-02 16:41:41 +0100

    add tailwind style to categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -18,9 +18,9 @@ const categories = [
 
 export default function Categories() {
     return (
-        <ul className="bookCard">
+        <ul className="w-full flex flex-wrap md:grid-cols-7 justify-center gap-4 text-white">
             {categories.map(category => {
-                return <Link key={category} to={`/categories/${category}`}>{category}</Link>
+                return <Link key={category} to={`/categories/${category}`}><li className="flex-1 bg-slate-950 p-4 cursor-pointer rounded-xl">{category}</li></Link>
             })}
         </ul>
     )
