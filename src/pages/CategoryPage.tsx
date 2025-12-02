ab2125cefb072ad5473cfdabca352c5331e36988
hemingus <heming.hanevik@gmail.com>
2025-12-02 16:43:19 +0100

    refactor categories page

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -5,16 +5,23 @@ import Categories from "../components/Categories";
 
 export default function CategoryPage() {
     const { topic } = useParams();
-    if (!topic) return <h1>Invalid category.</h1>
+    if (!topic) return (
+            <main className="bg-slate-900 flex flex-col items-center min-h-screen">
+                <header className="w-full text-amber-500 font-semibold text-center text-4xl p-8">
+                    <h1>Categories</h1>
+                </header>
+                <Categories />
+            </main>
+    )
     const { data, isLoading, isError, error } = useBooksByTopic(topic);
     if (isLoading) return <p>Loading...</p>
     if (isError) return <p>{error.toString()}</p>
     
     return (
-        <div>
+        <main className="bg-slate-900 flex flex-col justify-center items-center min-h-screen">
             <Categories />
-            <h1>{topic}</h1>
+            <h1 className="w-full text-amber-500 font-semibold text-center text-4xl p-8">{topic}</h1>
             <BookList booklist={data}/>
-        </div>
+        </main>
     )
 }
