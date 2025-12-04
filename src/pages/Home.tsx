9e36c5c18acbcfb2c555dfe6795c038076465a30
hemingus <heming.hanevik@gmail.com>
2025-12-04 16:51:08 +0100

    reworked Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,19 @@
-import BookList from "../components/BookList";
-import Loading from "../components/ui/Loading";
-import { useBooks } from "../hooks/useBookQuery";
+import { Link } from "react-router-dom";
+import Categories from "../components/Categories";
 
 export default function Home() {
-    const { data, isLoading, isError, error } = useBooks();
-    if (isLoading) return <Loading />
-    if (isError) return <p>{error.toString()}</p>
+
     
     return (
-        <main className="bg-slate-900 flex flex-col justify-center items-center">
+        <main className="bg-slate-900 flex flex-col items-center h-screen">
             <header className="w-full text-amber-500 font-semibold text-center text-4xl p-8">
                 <h1>Home</h1>
-                <Loading />
             </header>
-            <BookList booklist={data}/>
+            <Link to={"/search"}>
+                <p>Browse</p>
+            </Link>
+            <h2></h2>
+            <Categories />
         </main>
     )
 }
