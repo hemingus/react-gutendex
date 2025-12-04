8f3d44fbcfabc09814ec70cd21e8649571bd635e
hemingus <heming.hanevik@gmail.com>
2025-12-04 16:45:51 +0100

    add margin

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -6,11 +6,11 @@ interface BookListProps {
     booklist: Book[]
 }
 
-export default function BookList( { booklist }:BookListProps) {
+export default function BookList( { booklist = [] }:BookListProps) {
     const { isFavorite, addFavorite, removeFavorite } = useFavorites();
 
     return (
-        <div className="w-[90%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-stretch">
+        <div className="w-[90%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-stretch mb-8">
             {booklist.map((book) => {
                 return <BookCard 
                     key={book.id} book={book} 
