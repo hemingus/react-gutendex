280619afbdb1581a338ee902c5e0d733a4d6758e
hemingus <heming.hanevik@gmail.com>
2025-12-04 17:27:57 +0100

    add keys to loading spinner

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -22,7 +22,7 @@ export default function Loading() {
             <div className="relative w-[200px] h-[400px] mx-auto animate-spin-slow">
                 {categories.map((item, index) => {
                     const angle = (index / categories.length) * 360;
-                    return <li style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
+                    return <li key={item} style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
                     className="bg-slate-600 w-[50px] h-[50px] absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
                 })}
             </div>
