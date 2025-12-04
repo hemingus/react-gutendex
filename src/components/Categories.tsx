ad89908f08618fb8a45fc2181856784f96b10593
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-04 09:21:14 +0100

    add responsive grid, fix Link

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -18,17 +18,19 @@ const categories = [
 
 export default function Categories() {
     return (
-        <ul className="grid grid-cols-4 justify-center gap-4 text-white">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-4 text-white">
             {categories.map(category => {
-                return <li style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.9),rgba(0,0,0,0.5)), url('/assets/${category}.png`}}
-                        className={`border-2 border-[rgba(50,50,50,0.3)] text-gray-200 relative 
-                        flex justify-center items-center w-[250px] h-[140px] bg-cover bg-center 
-                        p-4 cursor-pointer rounded-xl  transition-all duration-200
-                        hover:scale-105 hover:text-white hover:shadow-[3px_3px_10px_1px_black]`}>
-                            <Link className="z-10 rounded-lg p-2 font-bold text-xl" key={category} to={`/categories/${category}`}>
-                                {category}
-                            </Link>
-                        </li>
+                return <Link  key={category} to={`/categories/${category}`}>
+                            <li 
+                            style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.9),rgba(0,0,0,0.5)), url('/assets/${category}.png`}}
+                            className={`border-2 border-[rgba(50,50,50,0.3)] text-gray-200 relative 
+                            flex justify-center items-center w-[250px] h-[140px] bg-cover bg-center 
+                            p-4 cursor-pointer rounded-xl  transition-all duration-200
+                            hover:scale-105 hover:text-white hover:shadow-[3px_3px_10px_1px_black]`}
+                            >
+                                <span className="z-10 rounded-lg p-2 font-bold text-xl">{category}</span>
+                            </li>
+                        </Link>
             })}
         </ul>
     )
