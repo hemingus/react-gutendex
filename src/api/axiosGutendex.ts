625ebd47a159de6ce1523949ef07f7a7f586fd57
hemingus <heming.hanevik@gmail.com>
2025-12-04 16:45:24 +0100

    fetchBooks now takes filter as params + remove fetchBooksByTopic

--- src/api/axiosGutendex.ts
+++ src/api/axiosGutendex.ts
@@ -1,17 +1,18 @@
 import axios from "axios";
+import type { BookFilters } from "../types/book";
 
 const BASE_URL = "https://gutendex.com";
 
-const fetchBooks = async () => {
-    const res = await axios.get(`${BASE_URL}/books`);
+const fetchBooks = async (filters: BookFilters & {page: number} = {page: 1}) => {
+    const res = await axios.get(`${BASE_URL}/books`, {
+        params: {
+            search: filters.search,
+            topic: filters.topic,
+            page: filters.page,
+        },
+    });
     console.log(res.data.results);
     return res.data.results; 
-}
-
-const fetchBooksByTopic = async ( topic: string ) => {
-    const res = await axios.get(`${BASE_URL}/books?topic=${topic}`)
-    console.log(res.data.results);
-    return res.data.results;
 }
 
 const fetchBookById = async ( id: string ) => {
@@ -20,4 +21,4 @@ const fetchBookById = async ( id: string ) => {
     return res.data;
 }
 
-export { fetchBooks, fetchBooksByTopic, fetchBookById }
+export { fetchBooks, fetchBookById }
