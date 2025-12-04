e8cefbd02e009e745b3064a28a4a9878ff44e41a
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-04 08:25:04 +0100

    add style

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -18,9 +18,17 @@ const categories = [
 
 export default function Categories() {
     return (
-        <ul className="w-full flex flex-wrap md:grid-cols-7 justify-center gap-4 text-white">
+        <ul className="grid grid-cols-4 justify-center gap-4 text-white">
             {categories.map(category => {
-                return <Link key={category} to={`/categories/${category}`}><li className="flex-1 bg-slate-950 p-4 cursor-pointer rounded-xl">{category}</li></Link>
+                return <li style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.9),rgba(0,0,0,0.5)), url('/assets/${category}.png`}}
+                        className={`border-2 border-[rgba(50,50,50,0.3)] text-gray-200 relative 
+                        flex justify-center items-center w-[250px] h-[140px] bg-cover bg-center 
+                        p-4 cursor-pointer rounded-xl  transition-all duration-200
+                        hover:scale-105 hover:text-white hover:shadow-[3px_3px_10px_1px_black]`}>
+                            <Link className="z-10 rounded-lg p-2 font-bold text-xl" key={category} to={`/categories/${category}`}>
+                                {category}
+                            </Link>
+                        </li>
             })}
         </ul>
     )
