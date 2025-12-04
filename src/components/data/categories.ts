3b6730a4108f956572dcdcaad122790b3db2ecac
hemingus <heming.hanevik@gmail.com>
2025-12-04 16:49:24 +0100

    add separate file for categories in data folder

---
+++ src/components/data/categories.ts
@@ -0,0 +1,15 @@
+export const categories = [
+    "Fiction",
+    "Mystery",
+    "Thriller",
+    "Romance",
+    "Fantasy",
+    "Morality",
+    "Society",
+    "Power",
+    "Justice",
+    "Adventure",
+    "Tragedy",
+    "War",
+    "Philosophy"
+]
