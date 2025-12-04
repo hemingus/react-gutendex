3e741fb6b146e0baad0eacc088f49221384d96b2
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-04 09:21:39 +0100

    change to animation

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -18,15 +18,15 @@ export default function Loading() {
     const radius = 150;
     return (
 
-        <div className="animate-blink-slow">
-            <div className="relative w-[400px] h-[400px] mx-auto animate-spin-slow">
+        <div>
+            <div className="relative w-[200px] h-[400px] mx-auto animate-spin-slow">
                 {categories.map((item, index) => {
                     const angle = (index / categories.length) * 360;
-                    return <li style={{ listStyle: "none", backgroundImage: `url(/assets/${item}.png)`, transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`}}
-                    className="bg-white w-[50px] h-[50px] absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text"></li>
+                    return <li style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
+                    className="bg-white w-[50px] h-[50px] absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
                 })}
             </div>
-            Loading...
+            <p className="animate-blink-slow">Loading...</p>
         </div>
     );
 }
