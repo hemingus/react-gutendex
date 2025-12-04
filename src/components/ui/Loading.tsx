1fac4e48f9cd705510b416c72ae9baec9b675b11
hemingus <heming.hanevik@gmail.com>
2025-12-04 16:49:44 +0100

    change style Loading component

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -23,7 +23,7 @@ export default function Loading() {
                 {categories.map((item, index) => {
                     const angle = (index / categories.length) * 360;
                     return <li style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
-                    className="bg-white w-[50px] h-[50px] absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
+                    className="bg-slate-600 w-[50px] h-[50px] absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
                 })}
             </div>
             <p className="animate-blink-slow">Loading...</p>
