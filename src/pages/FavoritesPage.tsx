ecbb1378aa0249feadfd5cdd166dc501d0128b4a
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-06 23:31:51 +0100

    adjust layout

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -5,7 +5,7 @@ export default function FavoritesPage() {
     const { favorites } = useFavorites();
 
     return (
-        <main className="bg-slate-900 flex flex-col justify-center items-center min-h-screen">
+        <main className="bg-slate-900 flex flex-col items-center min-h-screen">
             <header className="w-full text-amber-500 font-semibold text-center text-4xl p-8">
                 <h1>Favorites</h1>
             </header>
