e88fa876238c355ad52d3b080557e8df13d96ae1
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-07 23:53:37 +0100

    fix Loading component

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,28 +1,13 @@
-const categories = [
-"Fiction",
-"Mystery",
-"Thriller",
-"Romance",
-"Fantasy",
-"Morality",
-"Society",
-"Power",
-"Justice",
-"Adventure",
-"Tragedy",
-"War",
-"Philosophy"
-]
-
 export default function Loading() {
-    const radius = 50;
+    const count = 15;
+    const radius = 60;
     return (
 
         <div>
             <div className="relative h-[200px] mx-auto animate-spin-slow">
-                {categories.map((item, index) => {
-                    const angle = (index / categories.length) * 360;
-                    return <li key={item} style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
+                {Array.from({ length: count }).map((_, index) => {
+                    const angle = (index / count) * 360;
+                    return <li key={index} style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
                     className="bg-slate-600 w-5 h-5 absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
                 })}
             </div>
