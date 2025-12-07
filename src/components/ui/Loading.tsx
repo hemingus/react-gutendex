bd989fc8cac5b86b107adc567a36435cea9060b2
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-07 14:17:40 +0100

    change to Loading style

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -15,18 +15,18 @@ const categories = [
 ]
 
 export default function Loading() {
-    const radius = 150;
+    const radius = 50;
     return (
 
         <div>
-            <div className="relative w-[200px] h-[400px] mx-auto animate-spin-slow">
+            <div className="relative h-[200px] mx-auto animate-spin-slow">
                 {categories.map((item, index) => {
                     const angle = (index / categories.length) * 360;
                     return <li key={item} style={{ listStyle: "none", transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`, "--delay": `${index/10}s`} as React.CSSProperties}
-                    className="bg-slate-600 w-[50px] h-[50px] absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
+                    className="bg-slate-600 w-5 h-5 absolute top-1/2 left-1/2 bg-cover bg-center rounded-full -translate-x-1/2 -translate-y-1/2 text-sm animate-blink-slow"></li>
                 })}
             </div>
-            <p className="animate-blink-slow">Loading...</p>
+            <p className="text-lime-300 text-xl animate-blink-slow">Loading...</p>
         </div>
     );
 }
