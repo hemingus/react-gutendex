d646044d19ff781cc5d0a93e25b1fc11b8e357be
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-08 08:23:54 +0100

    update BookDetails style

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -3,6 +3,8 @@ import { useBookById } from "../hooks/useBookQuery";
 import type { Person } from "../types/book";
 
 export default function BookDetails() {
+    
+
     const { id } = useParams();
     if (!id) return <div>Invalid book ID.</div>
 
@@ -12,40 +14,62 @@ export default function BookDetails() {
     
     return (
         <div className="flex flex-col gap-4 w-full min-h-screen bg-slate-900 text-white items-center">
-            <h1 className="text-4xl text-amber-200">{data.title}</h1>
-            <div className="grid lg:grid-cols-2  items-center justify-center max-w-[1200px]">
+            <h1 className="text-4xl text-blue-300 p-4 w-full text-center bg-slate-800">{data.title}</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 items-start w-full max-w-[1200px]">
                 
                 
-                <img className="w-1/2" src={data.formats["image/jpeg"]} alt={`${data.title} cover`} />
-                <div className="col-span-3 lg:col-span-1">
+                
+                <img className="w-3/4 max-w-xs mx-auto object-contain p-4" src={data.formats["image/jpeg"]} alt={`${data.title} cover`} />
+                
+                   
+                <div className="flex flex-col gap-4 col-span-2 sm:col-span-1 p-4">
                     
-                    <label>Authors:</label>
-                    <ul>
-                        {data.authors.map((author: Person) => {
-                            return <li>{author.name}</li>
-                        })}
-                    </ul>
-                    <p>Downloads: {data.download_count}</p>
-                    <label>Categories: {data.subjects[0]}</label>
-                    <ul className="flex flex-col gap-1">
-                        {data.subjects.map((subject: string) => {
-                            return <li>{subject}</li>
-                        })}
-                    </ul>
-                    <label>Language:</label>
-                    <ul>
-                        {data.languages.map((language: string) => {
-                            return <li>{language}</li>
-                        })}
-                    </ul>
+                    <div>
+                        <h2 className="text-2xl text-amber-200 mb-2">Authors</h2>
+                        <ul>
+                            {data.authors.map((author: Person) => {
+                                return <li className="w-fit text-xl rounded-lg">{author.name}</li>
+                            })}
+                        </ul>
+                    </div>
+                    
+                    <div>
+                        <h2 className="text-2xl text-amber-200 mb-2">Subjects</h2>
+                        <ul className="flex flex-wrap gap-2">
+                            {data.subjects.map((subject: string) => {
+                                return <li className="w-fit bg-[rgba(0,0,0,0.5)] rounded-lg p-1.5">{subject}</li>
+                            })}
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 className="text-2xl text-amber-200 mb-2">Languages</h2>
+                        <ul>
+                            {data.languages.map((language: string) => {
+                                return <li className="w-fit bg-slate-700 rounded-lg p-1.5">{language}</li>
+                            })}
+                        </ul>
+                    </div>
+                    
+                    <div>
+                        <h2 className="text-2xl text-amber-200 mb-2">E-book</h2>
+                        <div className="flex flex-wrap gap-4">
+                            <a className="bg-blue-700 w-fit p-2 hover:bg-blue-600 rounded-lg" href={data.formats["text/html"]} target="_blank">HTML</a>
+                            <a className="bg-blue-700 w-fit p-2 hover:bg-blue-600 rounded-lg" href={data.formats["application/epub+zip"]} target="_blank">EPUB</a>
+                            <a className="bg-blue-700 w-fit p-2 hover:bg-blue-600 rounded-lg" href={data.formats["text/html"]} target="_blank">HTML</a>
+                            <a className="bg-blue-700 w-fit p-2 hover:bg-blue-600 rounded-lg" href={data.formats["text/html"]} target="_blank">HTML</a>
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className="flex flex-col gap-4 justify-end lg">
-                    <h2 className="text-2xl">Summary</h2>
+                <div className="mt-8 col-span-2 flex flex-col gap-4 justify-end p-4">
+                    <h2 className="text-2xl text-amber-200 mb-2">Summary</h2>
                     {data.summaries.map((s: string, index: number) => {
                         return <p key={index}>{s}</p>
                     })}
                 </div>
+                
             </div>
 
         </div>
