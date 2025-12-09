ce544b5ea5e0e2f118fa9e17ca960c9d21927bc4
Heming Hanevik <heming.hanevik@gmail.com>
2025-12-09 08:20:07 +0100

    add paragraph text and button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,20 @@ export default function Home() {
 
     
     return (
-        <main className="bg-slate-900 flex flex-col items-center min-h-screen pb-8">
+        <main className="bg-linear-to-r from-black via-slate-900 to-black flex flex-col items-center  gap-8 min-h-screen pb-8 text-white p-4">
             <header className="w-full text-amber-500 font-semibold text-center text-4xl p-8">
-                <h1>Home</h1>
+                <h1>Gutendex</h1>
             </header>
+            <p className="text-xl text-center">Discover thousands of classic, open-access e-books powered by Gutendex. <br/>
+                Search, browse, and read timeless literature!</p> 
+
             <Link to={"/search"}>
-                <p>Browse</p>
+                <button className="flex items-center gap-2 text-2xl bg-indigo-950 rounded-lg p-2 cursor-pointer border border-blue-500 hover:bg-indigo-900">
+                    Find books 
+                    <img className="w-8" src="/assets/magnifying-glass.png" alt="Magnifying glass icon" />
+                </button>
             </Link>
-            <h2></h2>
+            <p className="text-xl text-center">Explore curated categories to quickly find books that match your interests:</p>
             <Categories />
         </main>
     )
