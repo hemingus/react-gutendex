3896898d63b2c752c1370994d7f382eb5dd6e9b7
hemingus <heming.hanevik@gmail.com>
2025-12-09 10:19:10 +0100

    improve styles

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -5,8 +5,8 @@ export default function FavoritesPage() {
     const { favorites } = useFavorites();
 
     return (
-        <main className="bg-linear-to-r from-black via-slate-900 to-black flex flex-col items-center min-h-screen">
-            <header className="w-full text-amber-500 font-semibold text-center text-4xl p-8">
+        <main className="bg-linear-to-r from-black from-[-100%] via-slate-900 to-black to-[200%] flex flex-col items-center min-h-screen">
+            <header className="w-full text-blue-500 font-semibold text-center text-4xl p-8">
                 <h1>Favorites</h1>
             </header>
             <BookList booklist={favorites} />
