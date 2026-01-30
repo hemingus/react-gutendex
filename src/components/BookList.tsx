f5d0c24d69d7e0c39054071a9e0ca63c55302867
Heming Hanevik <heming.hanevik@gmail.com>
2026-01-30 08:41:03 +0100

    change grid breakpoints

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -10,7 +10,7 @@ export default function BookList( { booklist = [] }:BookListProps) {
     const { isFavorite, addFavorite, removeFavorite } = useFavorites();
 
     return (
-        <div className="w-[90%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-stretch mb-8">
+        <div className="w-[90%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center items-stretch mb-8">
             {booklist.map((book:Book) => {
                 return <BookCard 
                     key={book.id} book={book} 
